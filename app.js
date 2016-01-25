c73b1a8b75ad4ef821f321e8c317685ef8c42159
Enrico Murru <enrico.murru@gmail.com>
2016-01-25 10:10:09 +0100

    changed web server

--- app.js
+++ app.js
@@ -2,11 +2,6 @@
 var express = require('express');
 var app = express();
 app.get('/', function(req,res){
-
-  if (req.url === '/favicon.ico') {
-    res.status(404);
-    return res.send("No icon");
-  }
   res.status(200);
   res.set("Content-Type","application/json");
   res.send('{"status":"SF-Git is Running", "details":"https://github.com/enreeco/sf-git"}');
